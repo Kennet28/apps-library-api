b77ffdf011ec7f8e188f7d6d88d6f820773366db
Kenned Jaryd Avila Alcocer <javila@servidor.net>
2024-01-17 17:55:06 -0600

    first commit

---
+++ apps/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+class AppType(models.Model):
+    name= models.CharField(max_length=150)
+    
+class App (models.Model): 
+    name = models.CharField(max_length=150)
+    type = models.ForeignKey(AppType, on_delete=models.CASCADE)
+    version = models.CharField(max_length=255)
+    versionCode = models.IntegerField()
+    url = models.TextField()
+    icon = models.TextField()
+    description = models.TextField()
+    isPublished = models.BooleanField(False)
+    isFree=models.BooleanField(False)
